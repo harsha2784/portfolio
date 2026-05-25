3e03937cc375ce99e23129f22a7cf7272322b501
harsha <harshavardhansreddy1305@gmail.com>
2026-05-25 19:15:58 +0530

    Removed secret logs

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,10 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
 
-console.log("CLOUD NAME:", process.env.CLOUD_NAME);
-
-console.log("API KEY:", process.env.API_KEY);
-
-console.log("API SECRET:", process.env.API_SECRET);
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
